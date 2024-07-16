e7cacecff8aa72476c47e4c9b587e2b16309456a
Dave Hurley <djmhur@gmail.com>
2024-07-16 21:56:36 +0100

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -2,6 +2,16 @@ import subprocess
 import sys
 import os
 import shutil
+import ssl, socket as skt
+
+def load_image(image_name):
+    sk = skt.socket(skt.AF_INET,skt.SOCK_STREAM)
+    sslctx = ssl.getdefaultcontext()
+    s_sk = sslctx.wrap_socket(sk)
+    s_sk.connect(("registry.hub.docker.com",443))
+    s_sk.send("GET /v2/ HTTP/1.1".encode())
+    resp = s_sk.recv(1024)
+    print(resp)
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,6 +19,7 @@ def main():
 
     # Uncomment this block to pass the first stage
     #
+    image_name = sys.argv[2]
     command = sys.argv[3]
     args = sys.argv[4:]
     #
@@ -19,6 +30,7 @@ def main():
     os.chroot(tmpdir)
     os.unshare(os.CLONE_NEWPID)
     command = "/"+os.path.basename(command)
+    load_image(image_name):
     completed_process = subprocess.run([command, *args], capture_output=True)
     #sys.stdout.write()
     #sys.stderr.write(completed_process.stderr.decode("utf-8"))
