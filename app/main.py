762056b7c1f4358d9498e009f6617c8e35a1767d
Dave Hurley <djmhur@gmail.com>
2024-07-20 05:58:57 +0100

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -28,6 +28,8 @@ def recv_token(sk):
     
 def get_docker_auth_token(image,tag):
     #dauth_req = ulreq("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/"+image+":pull")
+    gai_info = socket.getaddrinfo("https://google.ie",443)
+    print("GAI",gai_info)
     dauth_req = ulreq("https://google.ie")
     print("URL:",dauth_req.full_url.encode())
     dauthf = urlopen(dauth_req)
