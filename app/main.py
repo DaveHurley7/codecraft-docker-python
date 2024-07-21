2fdae204335f6989c1abfcda649d67982435027e
Dave Hurley <djmhur@gmail.com>
2024-07-21 18:10:53 +0100

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def load_image(image_name):
         image_name = image_name[:tag_sep]
         del tag_sep
     auth_token = get_docker_auth_token(image_name,tag)
-    dreg_req = ulreq("https://registry.docker.io/v2/library/"+image_name+"/manifests/"+tag,
+    dreg_req = ulreq("https://registry.hub.docker.com/v2/library/"+image_name+"/manifests/"+tag,
                     headers = {
                         "Content-Type": "application/vnd.docker.distribution.manifest.v2+json",
                         "Authorization": "Bearer " + auth_token
