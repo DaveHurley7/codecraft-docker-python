ee097d3774237feded45d893279ed719375d06d5
Dave Hurley <djmhur@gmail.com>
2024-07-21 18:13:09 +0100

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ def load_image(image_name):
                     })
     dregf = urlopen(dreg_req)
     dreg_resp = dregf.read()
-    print(dreg_resp)
+    print(json.dumps(dreg_resp,indent=4))
     dregf.close()
     #dreg_sk = initsocktohost(("registry.hub.docker.com",443))
     #dreg.send("GET /v2/ HTTP/1.1".encode())
